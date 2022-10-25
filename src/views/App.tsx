965fbf07cf586f4835b0a47a12153e58f57a406d
James Tomlinson <jamest@stripe.com>
2022-10-25 21:54:03 +1100

    Working phase array selector

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,11 +1,13 @@
-import { Banner, Button, Box, ContextView, Inline, Link, TableHeaderCell, Table, TableHead, TableRow, TableBody, Spinner, Icon, List, ListItem } from "@stripe/ui-extension-sdk/ui";
+import { Banner, Button, Box, ContextView, Divider, FormFieldGroup, Inline, Link, TableHeaderCell, Table, TableHead, TableRow, TableBody, Spinner, Icon, List, ListItem, Badge, FocusView, Select, TextField, TableCell } from "@stripe/ui-extension-sdk/ui";
 import type { ExtensionContextValue } from "@stripe/ui-extension-sdk/context";
 
 import BrandIcon from "./brand_icon.svg";
 import { Stripe } from "stripe";
 import { createHttpClient, STRIPE_API_KEY } from '@stripe/ui-extension-sdk/http_client';
-import type { ExtensionContextValue } from "@stripe/ui-extension-sdk/context";
 import { useState, useEffect, useCallback } from 'react';
+import { produce } from 'immer';
+import { generate } from "shortid";
+import shortid from "shortid";
 /**
  * This is a view that is rendered in the Stripe dashboard's customer detail page.
  * In stripe-app.json, this view is configured with stripe.dashboard.customer.detail viewport.
@@ -21,14 +23,29 @@ const stripe = new Stripe(STRIPE_API_KEY, {
 
 
 const CustomerDetailView = ({ userContext, environment }: ExtensionContextValue) => {
+ 
   const BASE_URL =
     environment.mode == "test"
       ? `https://dashboard.stripe.com/${environment.mode}`
       : `https://dashboard.stripe.com`;
+  const [showDataInput, setShowDataInput] = useState<boolean>(false);
   const [schedules, setSchedules] = useState<Stripe.SubscriptionSchedule[]>();
+  const [prices, setPrices] = useState<Stripe.Price[]>();
+  const [coupons, setCoupons] = useState<Stripe.Coupon[]>();
+  const [items, setItems] = useState<Item[]>([]);
+  interface Item {
+    id:string
+    price: string
+    amount:string
+    productName:string
+    iterations:string
+    coupon:string
+  }
+  
 
+  //Get Current Subscription Schedules
   const getSchedules = useCallback(async () => {
-    const data = await stripe.subscriptionSchedules.list({customer:environment.objectContext?.id});
+    const data = await stripe.subscriptionSchedules.list({customer:environment.objectContext?.id,});
     setSchedules(data.data);
   }, []);
 
@@ -36,33 +53,177 @@ const CustomerDetailView = ({ userContext, environment }: ExtensionContextValue)
     getSchedules();
   }, [getSchedules]);
 
+//Get Products and Extend Prices
+const getPrices = useCallback(async () => {
+  const data = await stripe.prices.list({active:true, expand:['data.product']});
+  setPrices(data.data);
+}, []);
+
+useEffect(() => {
+  getPrices();
+}, [getPrices]);
+
+//Get Coupons
+const getCoupons = async () => {
+  const data = await stripe.coupons.list();
+  setCoupons(data.data);
+}
+
+useEffect(() =>{
+  getCoupons();
+}, [getCoupons]);
+
+//Create the Schedule
+const phases = parsePhaseArray(items);
+
 
 //View Components//
   return (
-    <ContextView title="Schediule View">
+    <ContextView 
+    title="Active Subscription Schedules"
+    description = {`Customer: ${userContext.account.name}`}
+    primaryAction = {<Button type="primary" onPress={() => setShowDataInput(true)}>Create Subscription Schedule</Button>}
+    >
+      {/*Home Page View*/}
       <Box
         css={{
-          padding:'large',
+          //padding:'large',
           backgroundColor:'container',
           borderRadius: 'small',
         }}>
           <List>
-            {schedules && schedules.map((schedules)=>(
-             
-              <ListItem
+            {schedules && schedules.map((schedule)=>(
+              
+             <ListItem
+              key={schedule.id.toString()}
               value={
-                <Button onPress={() => alert('edit')} type="primary" disabled='false'>
-                Edit
+                <Button type="primary">
+                  <Icon name = "external" size="xsmall"></Icon>
                 </Button>
               }
-              id={schedules.id}
-              title={<Box css={{fontSize:'xsmall'}}>Schedule {schedules.subscription}</Box>}
-              secondaryTitle={<Box>{schedules.status}</Box>}
+              title={<Box>{schedule.subscription}</Box>}
+              secondaryTitle={<Box>Status:{schedule.status}</Box>}
+              id={schedule.id}
               />
-            ))}
+))}
           </List>
-
       </Box>
+
+    {/* Create New Schedule View */}
+    <FocusView
+      title="Create new subscription schedule"
+      shown={showDataInput}
+      onClose={() => setShowDataInput(false)}
+      primaryAction={<Button type='primary' >Save</Button>}
+      secondaryAction={<Button onPress={() => setShowDataInput(false)}>Cancel</Button>}
+      >
+        
+       {items.map((i, index) => {
+        return (
+          <Box
+          css={{
+            width:'fill',
+            paddingY:'small',
+            //paddingRight:"xlarge",
+            background: "container",
+            marginY:"large",
+          }}
+          key={i.id}>
+            <Inline></Inline>
+            <Select 
+            name='product'
+            label='Select a product & price'
+            aria-label="select product and price"
+            onChange={(e) => {
+              const price = e.target.value;
+              setItems(currentItem => 
+                produce(currentItem, v => {
+                  v[index].price = price;
+                })
+              );
+            }} value={i.price}>
+              {prices?.map((price) => (
+                <option
+                key={price.id}
+                value={price.id}
+                id={price.id}>{`${price.product?.name} - ${price?.nickname} - ${("$"+ price?.unit_amount/100)}`}</option>
+              ))}
+            </Select>
+            <Box
+        css={{
+          paddingY:'small',
+          stack:'x',
+        }}>      
+          <Box css={{width:'1/2', paddingRight:'xsmall'}}>
+           <Select
+           onChange={(e) => {
+            const discount = e.target.value;
+            setItems(currentItem => 
+              produce(currentItem, v => {
+                v[index].coupon = discount;
+              })
+            );
+          }}
+            name="discount"
+            label="Select a discount to apply"
+          >
+            {coupons?.map((discount) => (
+              <option
+              key={discount.id}
+              value={discount.id}
+              id={discount.id}>{discount.name}</option>
+            ))}
+          </Select>
+          </Box>
+          <Box css={{width:'1/2', paddingLeft:'xsmall'}}>
+          <Select
+            name="cycle"
+            label="Number of iterations"
+            onChange={(e) => {
+              const iteration = e.target.value;
+              setItems(currentItem => 
+                produce(currentItem, v => {
+                  v[index].iterations = iteration;
+                })
+              );
+            }}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="ongoing">Ongoing</option>
+          </Select>
+          </Box>
+          
+      </Box>
+   
+          </Box>
+          
+        )})}
+  
+        <Box
+        css={{
+          paddingTop:'large',
+          paddingBottom:'large',
+        }}>
+          <Button
+          type="primary"
+          onClick={() => {
+            setItems(currentItem => [...currentItem, {
+              id:generate(),
+            }])
+          }}
+          css={{
+            width: 'fill',
+            alignX: 'center'
+          }}
+          >Add Phase</Button>
+        </Box>
+        <Box>{JSON.stringify(items)}</Box>
+      </FocusView>
     </ContextView>
   );
 };
